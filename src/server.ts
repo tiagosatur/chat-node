f6e0c098690fccdb3ec064aad2affe00840b0cda
tiagosatur <tiagosatur@gmail.com>
2021-04-22 12:24:18 -0300

    Add typeorm, sqlite3, setup routes, create controllers, entities, repositories, database and /settings route

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,11 @@
 import express from "express";
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Hello from chat app!" });
-});
-
-app.post("/users", (req, res) => {
-  return res.json({ message: "User registered!" });
-});
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("🚀 Server running on port ", 3333);
